f61b45d656ec9fca7e671771a6857360c9aeca81
PHOTONINT\nagagopiraju_n <nagagopiraju.nallagonda@photoninfotech.net>
2018-07-18 13:02:08 +0530

    store configuration

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/store'
 
 export default class App extends Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>EduRising</Text>
-			</View>
+			<Provider store={store}>
+				<View style={styles.container}>
+					<Text style={styles.welcome}>EduRising</Text>
+				</View>
+			</Provider>
 		);
 	}
 }
