b29b8130fb9a1fb5e10f9bc2b0389dfe516a8938
PHOTONINT\nagagopiraju_n <nagagopiraju.nallagonda@photoninfotech.net>
2018-07-19 12:43:53 +0530

    images upload

---
+++ src/stylesheets/loginStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+export default styles = StyleSheet.create({
+	label: {
+        flexDirection: 'row',
+        padding: 10
+    },
+    usernametextinput: {
+        flexDirection: 'column',
+        borderWidth:1,
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 15,
+    }
+});
