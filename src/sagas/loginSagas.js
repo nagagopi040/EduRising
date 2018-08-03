53fcde64a21c5110054940d4cef2100db126e4a1
Naga Gopi Raju Nallagonda <nagagopi401142@gmail.com>
2018-08-03 16:40:26 +0530

    Add files via upload

--- src/sagas/loginSagas.js
+++ src/sagas/loginSagas.js
@@ -1,20 +1,28 @@
 import { put, call } from 'redux-saga/effects'
 import axios from 'axios'
 
-import { LOGIN_REQUEST_FAILED, LOGIN_REQUEST_SUCCEDED } from '../../actions/login/loginActionTypes';
+import { LOGIN_REQUEST_FAILED, LOGIN_REQUEST_SUCCEDED } from '../actions/login/loginActionTypes';
 
 export function* loginAuth(action) {
     try {
-        //const res = yield call(axios.post, 'http://10.100.100.200:3000/users', action.userCredentials)
-        yield put({
-            type: LOGIN_REQUEST_SUCCEDED,
-            message: "Succesfully authenticated",
-            userDetails: {name: 'Naga Gopi', class: '9A', image: ''}
-        })
+        const res = yield call(axios.post, 'http://10.100.100.200:8080/users/auth', action.userCredentials)
+        console.log(action.userCredentials, res.data)
+        if(!res.data.serverError)
+            yield put({
+                type: LOGIN_REQUEST_SUCCEDED,
+                userDetails: res.data.userInfo ? res.data.userInfo : {},
+            })
+        else
+            yield put({
+                type: LOGIN_REQUEST_FAILED,
+                serverError: res.data.serverError,
+                message: "Authentication Failed",
+            })
     } catch (error) {
         yield put({
             type: LOGIN_REQUEST_FAILED,
-            error
+            serverError: error,
+            message: error.message,
         })
     }
 }
