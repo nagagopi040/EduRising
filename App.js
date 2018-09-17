136ef4c312405737a0b01baac1100873e698ec68
Naga Gopi Raju Nallagonda <nagagopi401142@gmail.com>
2018-09-17 19:54:46 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Provider } from 'react-redux';
+import { ActivityIndicator } from 'react-native'
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 import store from './src/store'
-import RootNavigator from './src/navigation/RootNavigatior'
-
-import LoginPage from './src/containers/login/LoginPage'
+import { rootNavigation } from './src/navigation/RootNavigation'
+import auth from './src/utilities/auth'
+import CONSTANT from './src/utilities/constant'
+import Loader from './src/components/common/Loader';
 
 export default class App extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			isRequesting : true,
+			isSignedIn : false
+		}
+	}
+ 	componentDidMount() {
+		auth.isSignedIn(CONSTANT.AUTHENTICATION.ACCESS_TOKEN)
+            .then( res => {
+                if(res){
+					this.setState({ isSignedIn: res, isRequesting : false })
+                } else {
+                    this.setState({ isSignedIn: false, isRequesting : false })
+                }
+            }).catch(err => {
+                this.setState({ serverError : err , isRequesting : false })
+            })
+	}
 	render() {
+		const { isSignedIn, isRequesting } = this.state
+		const RootNavigation = rootNavigation(isSignedIn)
+		if(isRequesting){
+			return <Loader />
+		}
 		return (
 			<Provider store={store}>
-				<RootNavigator />
+				<RootNavigation />
 			</Provider>
 		);
 	}
-}
+	}
