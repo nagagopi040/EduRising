2973bdcbf9c137d47b1ba0dd21de76a5f3ae0efc
nagagopi040 <nagagopi401142@gmail.com>
2018-09-19 17:39:14 +0530

    Revert "Notification Module Done."
    This reverts commit 3ed32b87a328a9126bd046169e86d6c27752f0ce.

--- src/components/notificationpage/notification.js
+++ src/components/notificationpage/notification.js
@@ -1,6 +1,5 @@
 import React,{ Component } from 'react';
-import {View,Text,Modal,Animated,Image,Dimensions,StyleSheet} from 'react-native';
-import notificationJson from '../../json/notificationJson';
+import {View,Text,Modal,Animated,Dimensions,StyleSheet} from 'react-native';
 import { withNavigation } from 'react-navigation';
 
 class Notification extends Component{
@@ -19,31 +18,7 @@ class Notification extends Component{
                         </View>
                     </View>
                     <View style={styles.notificationBody}>
-                        {notificationJson.today.map((data,key) => {
-                            return (
-                                <View style={{
-                                    borderColor:'#0093C7',
-                                    borderWidth:2,
-                                    borderRadius:10,
-                                    shadowColor:'#95C9F1',
-                                    shadowOffset: {width: 0.7,height: 0.7},
-                                    shadowOpacity:5,
-                                    shadowRadius:5,
-                                    flexDirection:'row',
-                                    marginVertical:Dimensions.get('window').height*0.03,
-                                    padding:10,
-                                    }}>
-                                    <View style={{flex:2,justifyContent:'center',alignItems:'center'}}>
-                                        <Image source = {require('../../images/notification.png')} style={{ width: Dimensions.get('window').width * 0.11,
-                                                    height: Dimensions.get('window').width * 0.11}} />
-                                    </View>
-                                    <View style={{flex:8,flexDirection:'column'}}>
-                                        <Text style={{fontSize:15,color:"#0093C7",marginBottom:10}}>{data.noteHeader}</Text>
-                                        <Text style={{fontSize:10}}>{data.note}</Text>
-                                    </View>
-                                    </View>
-                        )
-                        })}
+
                         </View>
                 </Animated.View>
         )
@@ -59,7 +34,7 @@ const styles=StyleSheet.create({
         top:0,
         left:0,
         flexDirection:'column',
-        // alignItems:'stretch'
+        alignItems:'stretch'
     },
     notificationHeader:{
         flexDirection:'row',
@@ -86,10 +61,7 @@ const styles=StyleSheet.create({
     },
     notificationBody:{
         backgroundColor:'white',
-        height:Dimensions.get('window').height*0.9,
-        justifyContent:'flex-start',
-        alignItems:'center',
-        padding:10
+        height:Dimensions.get('window').height*0.9
     }
 })
 
